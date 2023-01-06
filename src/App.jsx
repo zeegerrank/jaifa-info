63f70bc2a06c9d557cba65d0f3061336773cb402
zeegerrank <zeegerrank@gmail.com>
2023-01-06 09:13:38 +0700

    create statice router and pre-build pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import TestProduct from "./pages/TestProduct";
 
 function App() {
   return (
     <>
-      <TestProduct />
+      {/* NavsBar */}
+      <Navbar className="mb-3" bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="justify-content-end">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/vegetables">Vegetables</Nav.Link>
+            <Nav.Link href="/academy">Academy</Nav.Link>
+            <Nav.Link href="/about-us">About Us</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      {/* App Routes */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element="" />
+          <Route path="/product" element={<TestProduct />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
