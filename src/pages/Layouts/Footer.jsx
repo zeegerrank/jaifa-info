addec921400104196f086df3e60b72ff2b2fb770
zeegerrank <zeegerrank@gmail.com>
2023-01-11 00:14:31 +0700

    polish footer responsive, create social media icon

--- src/pages/Layouts/Footer.jsx
+++ src/pages/Layouts/Footer.jsx
@@ -1,25 +1,42 @@
-import React from "react";import { Container, Navbar, Row, Col, Stack, Nav } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Container, Navbar, Row, Col, Stack, Nav } from "react-bootstrap";
 
 export const Footer = () => {
   return (
     <footer>
-      <Navbar className=" align-items-xxl-end" bg="primary" variant="dark">
+      <Navbar
+        className=" align-items-xxl-end"
+        bg="primary"
+        variant="dark"
+        style={{ backgroundColor: "#75D9FD" }}>
         <Container>
           <Stack className="">
-            <Row className="mt-2">
-              <Col className="text-center">Social Media</Col>
+            <Row className="mt-2 justify-content-evenly">
+              <Col xs={1} className="text-center ">
+                <i
+                  style={{ color: "#13F21B" }}
+                  class="fa-3x fa-brands fa-line"></i>
+              </Col>
+              <Col xs={1} className="text-center ">
+                <a href="https://www.facebook.com/jaifafarm">
+                  <i
+                    style={{ color: "#0914D6" }}
+                    class=" fa-3x fa-brands fa-facebook"></i>
+                </a>
+              </Col>
             </Row>
             <span
-              className="m-3 rounded-1 w-100"
-              style={{ height: "3px", backgroundColor: "lightgray" }}
+              className="my-3 rounded-1 w-100"
+              style={{ height: "5px", backgroundColor: "#5D8CE2" }}
             />
-            <Row className=" d-flex justify-content-around">
-              <Col>
-                <Row className="row-cols-1">
+            <Row className="row-cols-1 row-cols-lg-3">
+              <Col className="text-center">
+                <Row className="row-cols-1 ">
                   <h6 className="col" style={{ textDecoration: "underline" }}>
                     Company
                   </h6>
-                  <p className=" col" style={{ textIndent: "30px" }}>
+                  <p className="col text-start" style={{ textIndent: "30px" }}>
                     fringilla ut morbi tincidunt augue interdum velit euismod in
                     pellentesque massa placerat duis ultricies lacus sed turpis
                     tincidunt id aliquet risus feugiat in ante metus dictum at
@@ -29,9 +46,13 @@ export const Footer = () => {
                   </p>
                 </Row>
               </Col>
-              <Col>
+              <span
+                className="col d-lg-none d-block my-3 mx-auto rounded-2 w-75 "
+                style={{ height: "2px", backgroundColor: "#5D8CE2" }}
+              />
+              <Col className="text-center">
                 <Row className="row-cols-1">
-                  <h6 className="col" style={{ textDecoration: "underline" }}>
+                  <h6 className="col " style={{ textDecoration: "underline" }}>
                     ประเภทสินค้าฟาร์ม
                   </h6>
                   <p className="col m-1">
@@ -52,7 +73,7 @@ export const Footer = () => {
                     <a
                       href="/products/charcoal"
                       style={{ textDecoration: "none", color: "#28252A" }}>
-                      ถ่ายไฟ และน้ำส้มควันไม้
+                      ถ่านไฟ และน้ำส้มควันไม้
                     </a>
                   </p>
                   <p className="col m-1">
@@ -64,11 +85,16 @@ export const Footer = () => {
                   </p>
                 </Row>
               </Col>
-              <Col>Contact</Col>
+              <span
+                className="col d-lg-none d-block my-3 mx-auto rounded-2 w-75 "
+                style={{ height: "2px", backgroundColor: "#5D8CE2" }}
+              />
+
+              <Col className="text-center">Contact</Col>
             </Row>
             <span
-              className="m-3 rounded-1 w-100"
-              style={{ height: "3px", backgroundColor: "lightgray" }}
+              className="my-3 rounded-1 w-100"
+              style={{ height: "5px", backgroundColor: "#5D8CE2" }}
             />
             <Row className="mb-2">
               <Col className="text-center">Designed by p-sek</Col>
